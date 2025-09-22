b5f735aa7c52f5634291d0c8eead5c636879978b
Berkaycan Akkoc <77438994+berkayakkocc@users.noreply.github.com>
2025-09-22 05:15:19 +0300

    Add EF Core health checks package
    Added Microsoft.Extensions.Diagnostics.HealthChecks.EntityFrameworkCore to support health checks for Entity Framework Core. Also updated Program.cs to include the necessary using directive.

--- Backend/QuickCRM.API/Program.cs
+++ Backend/QuickCRM.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using QuickCRM.Application.Services;
 using QuickCRM.Core.Interfaces;
 using QuickCRM.Infrastructure.Data;
